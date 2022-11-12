fe648a6f368590643b6d071f319baab89ec0ad05
T-r-i-p-l-e-A <75400069+aaryatopiwala@users.noreply.github.com>
2022-11-12 13:30:52 -0800

    Fill in grade.sh and TestListExamples

--- TestListExamples.java
+++ TestListExamples.java
@@ -1,6 +1,52 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.util.ArrayList;
+import java.util.List;
+
+class OddWordChecker implements StringChecker {
+  public boolean checkString(String s) {
+      return ((s.length() % 2) == 1);
+  }
+}
+
+class OWordChecker implements StringChecker {
+  public boolean checkString(String s) {
+      return s.contains("o") || s.contains("O");
+  }
+}
 
 public class TestListExamples {
   // Write your grading tests here!
+  @Test
+  public void testFilter() {
+      OddWordChecker odd = new OddWordChecker();
+      ArrayList<String> input1 = new ArrayList<>();
+      input1.add("o");
+      input1.add("aa");
+      input1.add("aaa");
+      ArrayList<String> output1 = new ArrayList<>();
+      output1.add("o");
+      output1.add("aaa");
+      List<String> expected = ListExamples.filter(input1, odd);
+      assertEquals(output1, expected);
+  }
+
+  @Test
+    public void testMerge() {
+        ArrayList<String> input1 = new ArrayList<>();
+        input1.add("a");
+        input1.add("c");
+        ArrayList<String> input2 = new ArrayList<>();
+        input2.add("b");
+        input2.add("d");
+        input2.add("e");
+        ArrayList<String> output1 = new ArrayList<>();
+        output1.add("a");
+        output1.add("b");
+        output1.add("c");
+        output1.add("d");
+        output1.add("e");
+        List<String> expected = ListExamples.merge(input1, input2);
+        assertEquals(output1, expected);
+    }
 }
--- grade.sh
+++ grade.sh
@@ -1,7 +1,21 @@
 # Create your grading script here
-
-set -e
+#set -e
+CPATH=".:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar"
 
 rm -rf student-submission
+
 git clone $1 student-submission
 
+cp TestListExamples.java student-submission
+cp -r lib student-submission
+cd student-submission
+
+javac -cp $CPATH *.java
+if [ $? -eq 0 ];
+then
+    echo "Files complied"
+else
+    echo "Files failed to compile"
+    exit 1
+fi
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples
